edf1f7b3132b4e68a7266b518f0658f3709480f3
AnubhavSaxena43 <anubhavsa@zaamo.co>
2021-12-12 19:45:12 +0530

    web android fix

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-syntax-jsx',
-    ['@babel/plugin-proposal-private-methods', {loose: true}],
-    ['@babel/plugin-proposal-private-property-in-object', {loose: true}],
-    ['@babel/plugin-proposal-class-properties', {loose: true}],
-  ],
+  plugins: ['@babel/plugin-syntax-jsx'],
 };

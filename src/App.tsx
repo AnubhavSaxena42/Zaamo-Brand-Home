b96d9344b728fa6ab57b0e085f83a4881ca51fbd
AnubhavSaxena43 <anubhavsa@zaamo.co>
2021-12-24 02:51:17 +0530

    Headers added Token Storage

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,35 @@ import TaggingPanelStack from './navigation/MainNavigator';
 import {HomeTabNavigator} from './navigation/MainNavigator';
 import {AuthorizationStack} from './navigation/MainNavigator';
 import {MainStackNavigator} from './navigation/MainNavigator';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import {getItemFromStorage} from './services/storage-service';
 // Initialize Apollo Client
-const client = new ApolloClient({
+
+const httpLink = createHttpLink({
   uri: 'https://beta.zaamo.co/graphql/',
+});
+const authLink = setContext(async (_, {headers}) => {
+  // get the authentication token from local storage if it exists
+  const token = await getItemFromStorage('Token');
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+
+      authorization: token ? `JWT ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 

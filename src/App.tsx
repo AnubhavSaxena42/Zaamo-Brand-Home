4ef4258b5feab38bfd8a71486704ef0e379bf8ca
AnubhavSaxena43 <anubhavsa@zaamo.co>
2021-12-29 16:26:10 +0530

    zaamo support payment details mutations

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ const authLink = setContext(async (_, {headers}) => {
   // get the authentication token from local storage if it exists
   const token = await getItemFromStorage('Token');
   const storeId = await getItemFromStorage('Store-ID');
+  ś;
   // return the headers to the context so httpLink can read them
   return {
     headers: {

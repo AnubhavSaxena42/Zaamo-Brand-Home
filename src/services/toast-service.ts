3a22f09a918383e46d21319f23eec576fa82407b
AnubhavSaxena43 <anubhavsa@zaamo.co>
2022-01-01 17:00:04 +0530

    Loaders added / Vouchers and collections

---
+++ src/services/toast-service.ts
@@ -0,0 +1,19 @@
+import {Get, Put, Post, Delete} from './api.service';
+
+import {Platform, ToastAndroid} from 'react-native';
+import Toast from 'react-native-simple-toast';
+class ToastService {
+  showToast = (text, bottom) => {
+    const commonToast = Platform.OS === 'android' ? ToastAndroid : Toast;
+    if (text) {
+      commonToast.showWithGravity(
+        text,
+        Toast.LONG,
+        bottom ? Toast.BOTTOM : Toast.CENTER,
+      );
+    }
+  };
+}
+const toastService = new ToastService();
+Object.freeze(toastService);
+export default toastService;
